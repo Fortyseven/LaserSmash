1fe5451b0024d71b8e72b01c8f4d4d4e4e691d7a
Fortyseven <fortyseven@gmail.com>
2014-03-23 14:04:30 -0400

    Added some loose files

--- Assets/GameTestControls.cs
+++ Assets/GameTestControls.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Game;
 
 public class GameTestControls : MonoBehaviour
 {
@@ -15,7 +16,56 @@ public class GameTestControls : MonoBehaviour
     void Update()
     {
         if (Input.GetKeyDown("1")) {
-            Debug.Log("spawn");
+            SpawnAsteroidLarge();
         }
+        if (Input.GetKeyDown("2")) {
+            SpawnAsteroidSmall();
+        }
+        if (Input.GetKeyDown("3")) {
+            SpawnUFO();
+        }
+        if (Input.GetKeyDown("4")) {
+            SpawnBombLg();
+        }
+        if (Input.GetKeyDown("5")) {
+            SpawnBombSm();
+        }
+        if (Input.GetKeyDown("6")) {
+            SpawnSatellite();
+        }
+    }
+
+    void SpawnAsteroidLarge()
+    {
+        Vector3 where = new Vector3( Random.Range( -GameConstants.SCREEN_X_BOUNDS, GameConstants.SCREEN_X_BOUNDS ), GameConstants.SCREEN_Y_GEN_OFFSET, 0 );
+        Instantiate( EnemyObjects[0], where, Quaternion.identity );
+    }
+
+    void SpawnAsteroidSmall()
+    {
+        Vector3 where = new Vector3( Random.Range( -GameConstants.SCREEN_X_BOUNDS, GameConstants.SCREEN_X_BOUNDS ), GameConstants.SCREEN_Y_GEN_OFFSET, 0 );
+        Instantiate( EnemyObjects[1], where, Quaternion.identity );
+    }
+
+
+    void SpawnUFO()
+    {
+        Vector3 where = new Vector3( Random.Range( -GameConstants.SCREEN_X_BOUNDS, GameConstants.SCREEN_X_BOUNDS ), GameConstants.SCREEN_Y_GEN_OFFSET, 0 );
+        Instantiate( EnemyObjects[2], where, Quaternion.identity );
+    }
+
+    void SpawnBombLg()
+    {
+        Instantiate(EnemyObjects[3], new Vector3(Random.Range(-GameConstants.SCREEN_X_BOUNDS,GameConstants.SCREEN_X_BOUNDS), GameConstants.SCREEN_Y_GEN_OFFSET, 0), Quaternion.identity);
+    }
+
+    void SpawnBombSm()
+    {
+        Instantiate(EnemyObjects[4], new Vector3(Random.Range(-GameConstants.SCREEN_X_BOUNDS,GameConstants.SCREEN_X_BOUNDS), GameConstants.SCREEN_Y_GEN_OFFSET, 0), Quaternion.identity);
+    }
+
+    void SpawnSatellite()
+    {
+        Debug.Log("Not implemented.");
     }
 }
