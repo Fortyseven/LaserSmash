f2c5580805084bd9f458df30fa59061aa1966a29
Fortyseven <fortyseven@gmail.com>
2014-09-12 02:15:40 -0400

    Added wave generator; created EnemyType with abstractions for all spawnables

---
+++ Assets/Scenes/GameScene/Scripts/EnemyType.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class EnemyType : MonoBehaviour
+{
+    public abstract void Respawn();
+    protected bool _is_ready = false;
+
+    public void Hibernate()
+    {
+        gameObject.SetActive( false );
+        transform.position.Set( 0, 0, 0 );
+        _is_ready = false;
+    }
+}
---
+++ Assets/Scenes/GameScene/Scripts/WaveController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaveController : MonoBehaviour
+{
+    public const int MAX_OBJECT_PER_WAVEDEF = 20;
+
+    [System.Serializable]
+    public class WaveDefinition {
+        public string Name;
+        public GameObject GameObjectPrefab;
+        public float[] LevelFrequencyLow;
+        public float[] LevelFrequencyHigh;
+        public ObjectPool Pool;
+    }
+
+    public WaveDefinition[] Waves;
+
+    private ObjectPool[] _pools;
+
+    public WaveController( GameObject parent_go )
+    {
+    }
+
+    public void Awake()
+    {
+        CreatePools();
+        Reset();
+    }
+
+    public void Reset()
+    {
+
+    }
+
+    public void Update()
+    {
+        for(int i = 0; i < Waves.Length; i++) {
+            GameObject g = Waves[i].Pool.GetInstance();
+            if (g == null) break;
+        }
+    }
+
+    void CreatePools()
+    {
+        _pools = new ObjectPool[Waves.Length];
+        for(int i = 0; i < Waves.Length; i++) {
+            Waves[i].Pool = new ObjectPool(Waves[i].GameObjectPrefab, MAX_OBJECT_PER_WAVEDEF);
+        }
+    }
+
+}
