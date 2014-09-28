144e1d8623f13256af62bea3e156eddddba2ba20
Fortyseven <fortyseven@gmail.com>
2014-09-28 09:37:19 -0400

    First version of seeker missile enemy

---
+++ Assets/Scenes/GameScene/Objects/Enemies/Seeker/Seeker.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Seeker : EnemyType
+{
+    private const float SPEED = 5.0f;
+
+    void Awake()
+    {
+
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        //Vector3 pos = transform.position;
+        //pos.y += Time.deltaTime * SPEED;
+        //transform.Translate(transform.forward);
+        if (transform.position.y > 0.5f ) {
+            transform.position = Vector3.MoveTowards(transform.position, GameController.instance.PlayerShip.transform.position, Time.deltaTime * 8 );
+        } else {
+            Destroy(this.gameObject);
+        }
+
+        Respawn();
+    }
+
+    public override void Respawn ()
+    {
+        if (transform.position.y > 0.5f ) {
+            transform.LookAt( GameController.instance.PlayerShip.transform );
+        }
+    }
+}
