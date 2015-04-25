0ce6a12258eebc996b2f23e6f754016dccd501c2
Fortyseven <fortyseven@gmail.com>
2015-04-25 09:14:18 -0400

    New state machine classes

--- Assets/Scenes/GameScene/Scripts/Utils/State/StateBehavior.cs
+++ Assets/Scenes/GameScene/Scripts/Utils/State/StateBehavior.cs
@@ -1,15 +1,27 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class StateBehavior : MonoBehaviour
 {
-    void Start()
-    {
+    public MonoBehaviour Parent { get; protected internal set; }
+    public StateMachine Machine { get; protected internal set; }
 
+    public virtual void Init()
+    {
     }
 
-    void Update()
+    public virtual void OnEnter()
     {
+    }
 
+    public virtual void OnExit()
+    {
+    }
+
+    public virtual void OnUpdate()
+    {
+    }
+
+    public virtual void OnFixedUpdate()
+    {
     }
 }
--- Assets/Scenes/GameScene/Scripts/Utils/State/StateMachine.cs
+++ Assets/Scenes/GameScene/Scripts/Utils/State/StateMachine.cs
@@ -1,20 +1,62 @@
 ﻿using System;
 using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class StateMachine : MonoBehaviour
 {
-    public void Init<T>()
+    public Enum ActiveState { get; private set; }
+
+    private Dictionary<Enum, StateBehavior> _state_behaviors;
+    private MonoBehaviour _owner;
+
+    public StateMachine()
     {
-        if ( !typeof( T ).IsEnum )
-            throw new UnityException( "State generic is not enum" );
-
-        Debug.Log( Enum.GetValues( typeof( T ) ) );
-
-
+        ActiveState = null;
     }
+
+    public void Init( MonoBehaviour owner )
+    {
+        _owner = owner;
+    }
+
+    public void SwitchStateTo( Enum state_enum )
+    {
+        if ( ActiveState != null ) {
+            _state_behaviors[ ActiveState ].OnExit();
+        }
+        ActiveState = state_enum;
+        _state_behaviors[ ActiveState ].OnEnter();
+    }
+
+    public void Awake()
+    {
+        _state_behaviors = new Dictionary<Enum, StateBehavior>( 0 );
+    }
+
+    public void AddState<TClass>( Enum id )
+    {
+        //StateBehavior be = Activator.CreateInstance<TClass>() as StateBehavior;
+        var be = _owner.gameObject.AddComponent( typeof( TClass ) ) as StateBehavior;
+
+
+        _state_behaviors.Add( id, be );
+
+        _state_behaviors[ id ].Parent = _owner;
+        _state_behaviors[ id ].Machine = this;
+        _state_behaviors[ id ].Init();
+    }
+
     public void Update()
     {
+        if ( ActiveState == null )
+            return;
+        _state_behaviors[ ActiveState ].OnUpdate();
+    }
 
+    public void FixedUpdate()
+    {
+        if ( ActiveState == null )
+            return;
+        _state_behaviors[ ActiveState ].OnFixedUpdate();
     }
 }
