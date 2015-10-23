79db8ac699985611abcc44532846a04f46e0b7a7
Fortyseven <fortyseven@gmail.com>
2015-10-23 14:34:44 -0400

    Fixed stage lighting; found better HSVtoRGB routine

--- Assets/Scenes/GameScene/Scripts/SkyboxLevelModifier.cs
+++ Assets/Scenes/GameScene/Scripts/SkyboxLevelModifier.cs
@@ -2,120 +2,39 @@
 
 public class SkyboxLevelModifier : MonoBehaviour
 {
+    private const float SCORE_WRAP_POINT = 10000;
+
     public void Update()
     {
-        //FIXME: int score = _game_environment.Score;
+        //FIXME: We don't need to run this every frame; scale it back
 
-        //float hue = ( 1.0f / 10000 ) * score;
-        float hue = 0.5f;
-        float adj_hue = hue % 1.0f;
-
-        RenderSettings.fogColor = HSVToRGB( adj_hue, 0.52f, 0.82f );
+        float hue = ( 1.0f / SCORE_WRAP_POINT ) * GameController.instance.GameEnv.Score;
+        float adj_hue = hue % 1.0f; // loop
+        //Debug.Log( adj_hue );
+        RenderSettings.fogColor = HSVtoRGB( adj_hue, 0.35f, 0.82f ); //HSVToRGB( adj_hue, 0.52f, 0.82f );
     }
 
-
-
-    private Color HSVToRGB( float h, float s, float v, float a = 1.0f )
+    // Yoinked from https://goo.gl/v1ui5E
+    public static Color32 HSVtoRGB( float h, float s, float v )
     {
-        Color rgb = new Color();
+        int H = (int)(h * 6);
+        float f = h * 6 - H;
+        float p = v * (1 - s);
+        float q = v * (1 - f * s);
+        float t = v * (1 - (1 - f) * s);
 
-        var chroma = s * v;
-        var hdash = h / 60.0f;
-        var x = chroma * ( 1.0f - Mathf.Abs( ( hdash % 2.0f ) - 1.0f ) );
+        float r=0;
+        float g=0;
+        float b=0;
 
-        if ( hdash < 1.0f ) {
-            rgb.r = chroma;
-            rgb.g = x;
+        switch ( H ) {
+            case 0: r = v; g = t; b = p; break;
+            case 1: r = q; g = v; b = p; break;
+            case 2: r = p; g = v; b = t; break;
+            case 3: r = p; g = q; b = v; break;
+            case 4: r = t; g = p; b = v; break;
+            case 5: r = v; g = p; b = q; break;
         }
-        else if ( hdash < 2.0f ) {
-            rgb.r = x;
-            rgb.g = chroma;
-        }
-        else if ( hdash < 3.0f ) {
-            rgb.g = chroma;
-            rgb.b = x;
-        }
-        else if ( hdash < 4.0f ) {
-            rgb.g = x;
-            rgb.b = chroma;
-        }
-        else if ( hdash < 5.0f ) {
-            rgb.r = x;
-            rgb.b = chroma;
-        }
-        else if ( hdash <= 6.0f ) {
-            rgb.r = chroma;
-            rgb.b = x;
-        }
-
-        var min = v - chroma;
-
-        rgb.r += min;
-        rgb.g += min;
-        rgb.b += min;
-        rgb.a = a;
-
-        return rgb;
+        return new Color32( (byte)( 255 * r ), (byte)( 255 * g ), (byte)( 255 * b ), 255 );
     }
-
-    //private static Color ColorFromHSV( float h, float s, float v, float a = 1 )
-    //{
-    //    // no saturation, we can return the value across the board (grayscale)
-    //    if ( Mathf.Approximately( s, 0 ) )
-    //        return new Color( v, v, v, a );
-
-    //    // which chunk of the rainbow are we in?
-    //    float sector = h / 60;
-
-    //    // split across the decimal (ie 3.87 into 3 and 0.87)
-    //    int i = (int)sector;
-    //    float f = sector - i;
-
-    //    float p = v * ( 1 - s );
-    //    float q = v * ( 1 - s * f );
-    //    float t = v * ( 1 - s * ( 1 - f ) );
-
-    //    // build our rgb color
-    //    Color color = new Color( 0, 0, 0, a );
-
-    //    switch ( i ) {
-    //        case 0:
-    //            color.r = v;
-    //            color.g = t;
-    //            color.b = p;
-    //            break;
-
-    //        case 1:
-    //            color.r = q;
-    //            color.g = v;
-    //            color.b = p;
-    //            break;
-
-    //        case 2:
-    //            color.r = p;
-    //            color.g = v;
-    //            color.b = t;
-    //            break;
-
-    //        case 3:
-    //            color.r = p;
-    //            color.g = q;
-    //            color.b = v;
-    //            break;
-
-    //        case 4:
-    //            color.r = t;
-    //            color.g = p;
-    //            color.b = v;
-    //            break;
-
-    //        default:
-    //            color.r = v;
-    //            color.g = p;
-    //            color.b = q;
-    //            break;
-    //    }
-
-    //    return color;
-    //}
 }
