be6270d4b8f1ea340db15b6f5d4d1aed083c188d
Fortyseven <fortyseven@gmail.com>
2017-12-04 01:37:57 -0500

    Added debouncing class; added to Pause states

---
+++ Assets/Scripts/DebounceButton.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Game
+{
+    public class DebounceButton
+    {
+        private bool _start_state;
+        private string _button_name;
+
+        public DebounceButton( string button_name )
+        {
+            _button_name = button_name;
+            this.reset();
+        }
+
+        public void reset()
+        {
+            _start_state = true;
+        }
+
+        public bool isPressed()
+        {
+            var cur_state = Input.GetButton(_button_name);
+
+            if( _start_state && cur_state ) {
+                return false;
+            }
+            _start_state = false;
+            return cur_state;
+        }
+    }
+}
